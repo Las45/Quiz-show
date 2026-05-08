7c68d447282885dc0c6948fd00668c57bdc0206f
apple-schlecht <andrejstanic2009@gmail.com>
2026-05-08 10:01:23 +0200

    Added_Better_Main_Menu

---
+++ usercontrols/Icons/Shop.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_show.usercontrols.Icons
+{
+    /// <summary>
+    /// Interaktionslogik für Shop.xaml
+    /// </summary>
+    public partial class Shop : UserControl
+    {
+        public Shop()
+        {
+            InitializeComponent();
+        }
+        private void Shop_rect_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Shop_rect1.Stroke = Brushes.LightBlue;
+            Shop_rect1.StrokeThickness = 2;
+        }
+
+        private void Shop_rect_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Shop_rect1.StrokeThickness = 0;
+
+        }
+
+        private void Shop_rect_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
